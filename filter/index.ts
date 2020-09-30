ee24cd98fced57d5f7386188e412716b9cb56963
zcs <530627552@qq.com>
2020-09-30 17:30:10 +0800

    desc

--- filter/index.ts
+++ filter/index.ts
@@ -3,12 +3,18 @@ import { writeFileSync } from 'fs';
 import { read, Case } from '../collect-companys-detail';
 
 interface Lawer {
-  name: string;
-  url: string;
-  selfCaseNum: number;
-  companyCaseNum: number;
-  haidianCourtNum: number;
-  cases: Omit<Case, 'lawer'>[];
+  belongCompany: {
+    name: string;
+    url: string;
+  };
+  inheritCaseStatics: {
+    all: number;
+    haidianCourt: number;
+    '2020': number;
+    '2019': number;
+    '2018': number;
+  };
+  inheritCases: Omit<Case, 'lawer'>[];
 }
 interface Lawers {
   [name: string]: Lawer;
@@ -18,7 +24,6 @@ const filter = async (keywords: string[] = [], years: string[] = []) => {
   let allLawers: Lawers = {};
   detail.forEach(e => {
     const lawers: Lawers = {};
-    const companyCaseNum = e.cases.length;
     e.cases.forEach(ee => {
       let lawer = ee.lawer;
       if (lawer.includes('（') || lawer.includes('(')) {
@@ -29,46 +34,90 @@ const filter = async (keywords: string[] = [], years: string[] = []) => {
         lawer += '_' + index++;
       }
       delete ee.lawer;
-      if (!lawers[lawer]) {
-        lawers[lawer] = {
-          name: e.name,
-          url: e.url,
-          companyCaseNum,
-          haidianCourtNum: 0,
-          selfCaseNum: 0,
-          cases: [],
-        };
-      }
-      lawers[lawer].selfCaseNum += 1;
-      if (ee.court.includes('北京市海淀区人民法院')) {
-        lawers[lawer].haidianCourtNum += 1;
-      }
       if (
         (keywords.length === 0 ||
           keywords.some(keyword => ee.name.includes(keyword))) &&
         (years.length === 0 || years.some(year => ee.time === year))
       ) {
-        lawers[lawer].cases.push(ee);
+        if (!lawers[lawer]) {
+          lawers[lawer] = {
+            belongCompany: {
+              name: e.name,
+              url: e.url,
+            },
+            inheritCaseStatics: {
+              all: 0,
+              haidianCourt: 0,
+              '2020': 0,
+              '2019': 0,
+              '2018': 0,
+            },
+            inheritCases: [],
+          };
+        }
+        lawers[lawer].inheritCases.push(ee);
+
+        lawers[lawer].inheritCaseStatics.all += 1;
+        if (ee.court.includes('北京市海淀区人民法院')) {
+          lawers[lawer].inheritCaseStatics.haidianCourt += 1;
+        }
+        if (ee.time === '2020') {
+          lawers[lawer].inheritCaseStatics['2020'] += 1;
+        }
+        if (ee.time === '2019') {
+          lawers[lawer].inheritCaseStatics['2019'] += 1;
+        }
+        if (ee.time === '2018') {
+          lawers[lawer].inheritCaseStatics['2018'] += 1;
+        }
+      } else {
+        return [];
       }
     });
+
     allLawers = { ...allLawers, ...lawers };
+  });
+  Object.values(allLawers).forEach(e => {
+    e.inheritCases.sort((a, b) => {
+      if (a.court.includes('海淀') && !b.court.includes('海淀')) {
+        return -1;
+      }
+      if (b.court.includes('海淀') && !a.court.includes('海淀')) {
+        return 1;
+      }
+      return Number(b.time) - Number(a.time);
+    });
   });
   const entries = Object.entries(allLawers);
   entries.sort((a, b) => {
-    if (b[1].cases.length !== a[1].cases.length) {
-      return b[1].cases.length - a[1].cases.length;
+    let sub: number = 0;
+    sub = b[1].inheritCases.length - a[1].inheritCases.length;
+    if (sub !== 0) {
+      return sub;
     }
-    if (b[1].haidianCourtNum !== a[1].haidianCourtNum) {
-      return b[1].haidianCourtNum - a[1].haidianCourtNum;
+    sub =
+      b[1].inheritCaseStatics.haidianCourt -
+      a[1].inheritCaseStatics.haidianCourt;
+    if (sub !== 0) {
+      return sub;
     }
-    if (b[1].selfCaseNum !== a[1].selfCaseNum) {
-      return b[1].selfCaseNum - a[1].selfCaseNum;
+    sub = b[1].inheritCaseStatics['2020'] - a[1].inheritCaseStatics['2020'];
+    if (sub !== 0) {
+      return sub;
     }
-    return b[1].companyCaseNum - a[1].companyCaseNum;
+    sub = b[1].inheritCaseStatics['2019'] - a[1].inheritCaseStatics['2019'];
+    if (sub !== 0) {
+      return sub;
+    }
+    sub = b[1].inheritCaseStatics['2018'] - a[1].inheritCaseStatics['2018'];
+    if (sub !== 0) {
+      return sub;
+    }
+    return sub;
   });
   writeFileSync(
     path.join(process.cwd(), 'lawer.json'),
     JSON.stringify(entries, null, 2),
   );
 };
-filter(['遗嘱']);
+filter([], ['2018', '2019', '2020']);
